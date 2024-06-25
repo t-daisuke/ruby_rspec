652cbb874832e9983121a17eaa6ecae5aaf7c78e
t-daisuke <t.daisuke.64@gmail.com>
2024-06-25 16:03:13 +0900

    いらなくなったテストなので消します記念に残す

--- spec/lib/closeSet_spec.rb
+++ spec/lib/closeSet_spec.rb
@@ -1,9 +1,9 @@
 describe CloseSet do
-  describe 'いいはず' do
-    it do
-      expect(1).to eq 1
-    end
-  end
+  # describe 'いいはず' do
+  #   it do
+  #     expect(1).to eq 1
+  #   end
+  # end
   # ダメだった！(嬉しい)
   # describe 'ダメなはず' do
   #   it do
